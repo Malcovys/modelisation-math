45c1bd28175967cf375682ed33dd50bb121fa30c
malcovys <malcovys22.aps@gmail.com>
2025-11-07 16:57:24 +0300

    Implement linear programming functions and example usage with CSV data

---
+++ core/linear_programmation.py
@@ -0,0 +1,130 @@
+from pulp import LpProblem, LpMaximize, LpMinimize, LpVariable, LpStatus, value, LpAffineExpression
+import pandas as pd
+
+def lp_data_from_csv(
+    path: str,
+    to_maximize: str,
+    decision_var_col: str
+) -> dict[str, list]:
+    # Load csv data
+    data_frame = pd.read_csv(path)
+    data_frame_without_last_row = data_frame.iloc[:-1, :]  # Ignore the last row (ressouces row)
+
+    # Get decision variables
+    decision_vars: list[str] = data_frame_without_last_row[decision_var_col].tolist()
+
+    # Get decision variables coefficients
+    decision_vars_coef: list[float] = data_frame_without_last_row[to_maximize].tolist()
+
+    # Get resource headers (exclude decision_var_col and to_maximize)
+    exclude_columns = {decision_var_col, to_maximize}
+    ressources_headers: list[str] = [col for col in data_frame.columns if col not in exclude_columns]
+
+    # Get constraints coefficients (organized by resource, not by product)
+    constraintes_coef: list[list[float]] = [[] for _ in range(len(ressources_headers))]
+    for ressource_index, ressource_header in enumerate(ressources_headers):
+        for value in data_frame_without_last_row[ressource_header]:
+            constraintes_coef[ressource_index].append(float(value))
+
+    # Get inequality constraints (last row)
+    constraintes_inequality: list[float] = [
+        float(value) for value in data_frame.iloc[-1, :][ressources_headers]
+    ]
+
+    return {
+        "decision_vars": decision_vars,
+        "decision_vars_coef": decision_vars_coef,
+        "constraintes_coef": constraintes_coef,
+        "constraintes_inequality": constraintes_inequality
+    }
+
+
+def lp_solve(
+    problem_name: str | None,
+    decision_vars: list[str], 
+    decision_vars_coef: list[float],
+    constraintes_coef: list[list[float]],
+    constraintes_inequality: list[float],
+    maximize: bool,
+) -> dict[str, float]:
+    # Define problem name
+    name = problem_name 
+    if name is None:
+        name =  "lp_problem"
+
+    # Instancie the LpProblem model
+    lp_prob = LpProblem(name, LpMaximize if maximize else LpMinimize)
+
+    # Define decision variables
+    decision_variables: dict[str, LpVariable] = dict()
+
+    for i in range(len(decision_vars)):
+        decision_variables.update({
+            f"x{i+1}": LpVariable(decision_vars[i], lowBound=0)
+        })
+    # print(decision_variables)
+
+    # Set Objective function
+    objective_func = LpAffineExpression()
+    for index in range(len(decision_vars)):
+        objective_func += decision_vars_coef[index] * decision_variables[f"x{index+1}"]
+
+    lp_prob += objective_func
+
+    # Set Constraintes
+    # print(constraintes_coef)
+    for i in range(len(constraintes_coef)):
+        constrainte = LpAffineExpression()
+
+        for j in range(len(decision_vars)):
+            constrainte += constraintes_coef[i][j] * decision_variables[f"x{j+1}"]
+        
+        lp_prob += constrainte <= constraintes_inequality[i]
+    
+    # print(lp_prob)
+
+    # Resolve
+    solve_status = lp_prob.solve()
+    print(LpStatus[solve_status])
+
+    # Set solutions
+    solutions: dict[str, float] = dict()
+    for name, val in decision_variables.items():
+        solutions.update({val.name: value(val)})
+    
+    return solutions
+
+
+def lp_maximize_from_csv(
+    path: str,
+    problem_name: str | None,
+    to_maximize: str,
+    decision_var_col: str
+) -> dict[str, float]:
+    data = lp_data_from_csv(path, to_maximize, decision_var_col)
+
+    return lp_solve(
+        problem_name=problem_name,
+        decision_vars=data["decision_vars"],
+        decision_vars_coef=data["decision_vars_coef"],
+        constraintes_coef=data["constraintes_coef"],
+        constraintes_inequality=data["constraintes_inequality"],
+        maximize=True,
+    )
+
+def lp_minimize_from_csv(
+    path: str,
+    problem_name: str | None,
+    to_minimize: str,
+    decision_var_col: str
+) -> dict[str, float]:
+    data = lp_data_from_csv(path, to_minimize, decision_var_col)
+
+    return lp_solve(
+        problem_name=problem_name,
+        decision_vars=data["decision_vars"],
+        decision_vars_coef=data["decision_vars_coef"],
+        constraintes_coef=data["constraintes_coef"],
+        constraintes_inequality=data["constraintes_inequality"],
+        maximize=False,
+    )
--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 from core.linear_system import solve_linear_system
+from core.linear_programmation import lp_maximize_from_csv, lp_minimize_from_csv
+
 from typing import List
+import os
 
-a: List[List[float]] = [
-    [2,   25,  5],
-    [10,   0, 30],
-    [0.5, 10,  1]
-]
 
-b = [50, 100, 20]
+# a: List[List[float]] = [
+#     [2,   25,  5],
+#     [10,   0, 30],
+#     [0.5, 10,  1]
+# ]
 
-solutions = solve_linear_system(a, b)
+# b = [50, 100, 20]
 
-if(solutions is None):
-    print("Undetermined solution")
-else:
-    print(solutions)
+# solutions = solve_linear_system(a, b)
+
+# if(solutions is None):
+#     print("Undetermined solution")
+# else:
+#     print(solutions)
+
+currend_directory = os.path.dirname(os.path.abspath(__file__))
+pastry_data__path = os.path.join(currend_directory, "./data/csv/pastry.csv")
+piece_data__path = os.path.join(currend_directory, "./data/csv/piece.csv")
+
+# max_result = lp_maximize_from_csv(
+#     path=data_path, 
+#     problem_name="Pastry",
+#     to_maximize="Benefits",
+#     decision_var_col="Products"
+# )
+
+# print(f"max solution: {max_result}")
+
+min_result = lp_minimize_from_csv(
+    path=piece_data__path, 
+    problem_name="Pieces production",
+    to_minimize="Cost",
+    decision_var_col="Products"
+)
+
+print(f"min solution: {min_result}")
